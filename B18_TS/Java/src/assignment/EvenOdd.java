dd1d83c05c1c0089d06b04b77e0c2b0023bff387
Ashwini1301 <108318493+Ashwini1301@users.noreply.github.com>
2022-06-28 10:03:34 +0530

    Add files via upload

---
+++ B18_TS/Java/src/assignment/EvenOdd.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int  n = 50;
+	  	if (n%2==0){
+	    System.out.println("Even");
+	    }
+	    else{
+	    System.out.println("Odd");
+	    }
+	}
+
+}
